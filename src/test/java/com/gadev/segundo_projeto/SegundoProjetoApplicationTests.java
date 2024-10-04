0041fff77d6787346aa0663c724466ec57b873f6
Gabriel S A Ferreira <165482621+gabrielamoorim@users.noreply.github.com>
2024-10-04 10:12:55 -0300

    Category memory

---
+++ src/test/java/com/gadev/segundo_projeto/SegundoProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gadev.segundo_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
