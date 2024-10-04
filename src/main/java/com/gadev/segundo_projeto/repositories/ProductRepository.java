01d8b5de6f38015644945334b30ba89959af2bff
Gabriel S A Ferreira <165482621+gabrielamoorim@users.noreply.github.com>
2024-10-04 16:48:27 -0300

    Product memory

---
+++ src/main/java/com/gadev/segundo_projeto/repositories/ProductRepository.java
@@ -0,0 +1,31 @@
+package com.gadev.segundo_projeto.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.gadev.segundo_projeto.entities.Product;
+
+@Component
+public class ProductRepository {
+
+	private Map<Long, Product> map = new HashMap<>();
+
+	public void save(Product obj) {
+		map.put(obj.getId(), obj);
+	}
+
+	public Product findById(Long id) {
+
+		return map.get(id);
+	}
+
+	public List<Product> findAll() {
+
+		return new ArrayList<Product>(map.values());
+	}
+
+}
---
+++ src/main/java/com/gadev/segundo_projeto/resources/ProductResource.java
@@ -0,0 +1,36 @@
+package com.gadev.segundo_projeto.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gadev.segundo_projeto.entities.Product;
+import com.gadev.segundo_projeto.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductRepository categoryRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> list = categoryRepository.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id){
+		
+		Product cat = categoryRepository.findById(id);
+		
+		return ResponseEntity.ok().body(cat);
+	}
+}
